60cbf5c2b273ea5313f6aa4fe2a6e683a00e959f
Farah <Farah@DESKTOP-4IKORCG.lan>
2025-01-30 15:49:06 -0600

    implemented addition

--- src/Demo1/DemoTest1.java
+++ src/Demo1/DemoTest1.java
@@ -4,6 +4,11 @@ public class DemoTest1 {
 	public static void main(String args []) {
 		System.out.println("it's a beautiful day in the neighborhood");
 		//need more practice
+		
+		int a = 10;
+		int b= 15;
+		int result = a + b;
+		System.out.println(result);
 	}
 
 }
